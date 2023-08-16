195312b56f241dee95ed7d335ca2ab078431bb25
Tanim1999 <ihtanim56@gmail.com>
2023-08-17 00:44:17 +0600

    completed tasks

--- js/fontsize.js
+++ js/fontsize.js
@@ -1,7 +1,15 @@
-// const textArea=document.getElementById("text-area")
-// const numberInputField= document.getElementById("numberInput")
-// numberInputField.addEventListener("input",function(){
-//    const fontSize=numberInputField.value
-//    textArea.style.fontSize= `${fontSize}px`
-// })
-// console.log(number)
+const textArea=document.getElementById("text-area")
+
+const numberInputField= document.getElementById("numberInput")
+
+numberInputField.addEventListener("input",function(){
+   const fontSize=numberInputField.value
+   textArea.style.fontSize= `${fontSize}px`
+})
+
+
+const colorInputField=document.getElementById("colorInput")
+colorInputField.addEventListener("input",function(){
+    const textColor=colorInputField.value
+    textArea.style.color=`${textColor}`
+})
