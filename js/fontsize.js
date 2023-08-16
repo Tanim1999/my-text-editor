e906bc1dba55d8a949a267886b1c5fcd9d782179
Tanim1999 <ihtanim56@gmail.com>
2023-08-17 00:26:05 +0600

    zzzz

---
+++ js/fontsize.js
@@ -0,0 +1,7 @@
+// const textArea=document.getElementById("text-area")
+// const numberInputField= document.getElementById("numberInput")
+// numberInputField.addEventListener("input",function(){
+//    const fontSize=numberInputField.value
+//    textArea.style.fontSize= `${fontSize}px`
+// })
+// console.log(number)
